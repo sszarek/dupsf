a12d3e88b15d1d22abc38ea9cdbfd32cd054e562
Stefan Szarek <stefan.szarek@gmail.com>
2016-05-20 18:51:14 +0200

    Draft version of file comparison - to be refactored

--- index.js
+++ index.js
@@ -10,38 +10,83 @@ if (args.length === 0) {
 
 let dir = args[0];
 let fileMap = new Map();
+let duplicates = [];
 
 processDirectory(dir, err => {
-    console.error(err);
-    console.log('end');
+    if (err) {
+        return console.error(err);
+    }
+
+    findDuplicatesInBuckets();
 });
 
 function processDirectory(dir, cb) {
     fs.stat(dir, (err, stats) => {
         if (err) {
             if (err.code === 'ENOENT') {
-                return console.error(`File: ${dir} does not exist.`);
+                console.error(`File: ${dir} does not exist.`);
+                cb();
             } else {
                 return cb(err);
             }
         }
+
         if (stats.isDirectory()) {
-            console.log(dir);
             fs.readdir(dir, (err, files) => {
                 async.each(files, (file, cb) => processDirectory(path.join(dir, file), cb), cb);
             });
         } else if (stats.isFile()) {
-            console.log(dir);
-            processFileContents(dir, stats);
+            addFileToSizeBucket(dir, stats);
             cb();
         }
     });
 }
 
-function processFileContents(file, stats) {
+function addFileToSizeBucket(file, stats) {
     if (!fileMap.has(stats.size)) {
         fileMap.set(stats.size, []);
-    } 
-    
+    }
+
     fileMap.get(stats.size).push(file);
+}
+
+function findDuplicatesInBuckets(cb) {
+    console.log(fileMap);
+    async.each(fileMap, (bucket, cb) => {
+        if (bucket.length === 1) {
+            return cb();
+        }
+
+        let fileSize = bucket[0];
+        let bucketFiles = bucket[1];
+
+        async.whilst(() => bucketFiles.length > 1, compareFiles);
+
+        function compareFiles(cb) {
+            let firstPath = bucketFiles.pop();
+            console.log(bucketFiles);
+            fs.open(firstPath, 'r', (err, firstFd) => {
+                async.each(bucketFiles, (secondPath, eachCb) => {
+                    fs.open(secondPath, 'r', (err, secondFd) => {
+                        let firstBuffer = new Buffer(1024);
+                        let secondBuffer = new Buffer(1024);
+                        let bytesRead = 0;
+                        async.whilst(() => bytesRead < fileSize, cb => {
+                            async.parallel([
+                                cb => fs.read(firstFd, firstBuffer, 0, 1024, null, cb),
+                                cb => fs.read(secondFd, secondBuffer, 0, 1024, null, cb) 
+                            ], (err, data) => {
+                                //console.dir(data);
+                                cb(data);
+                            });
+                        }, () => {
+                            console.log(`Files processed: ${firstPath}, ${secondPath}`);
+                            return eachCb();
+                        });
+                    });
+                }, cb);
+            });
+
+        }
+    }, cb);
 }
