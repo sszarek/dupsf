1cbbf8cd3567070fdb44bae347e9090f233ef1d8
Stefan Szarek <stefan.szarek@gmail.com>
2016-05-30 13:30:39 +0200

    directory traversing moved to separate module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const async = require('async');
 const compare = require('./lib/compare');
+const directory = require('./lib/directory');
+
 const args = Array.prototype.slice.call(process.argv, 2);
 
 if (args.length === 0) {
@@ -13,7 +15,11 @@ let dir = args[0];
 let fileMap = new Map();
 let duplicates = [];
 
-processDirectory(dir, err => {
+directory.traverseDirectoryTree(dir, (dir, stats) => {
+    if (stats.isFile()) {
+        addFileToSizeBucket(dir, stats);
+    }
+}, err => {
     if (err) {
         return console.error(err);
     }
@@ -23,28 +29,6 @@ processDirectory(dir, err => {
         console.log();
     });
 });
-
-function processDirectory(dir, cb) {
-    fs.stat(dir, (err, stats) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                console.error(`File: ${dir} does not exist.`);
-                cb();
-            } else {
-                return cb(err);
-            }
-        }
-
-        if (stats.isDirectory()) {
-            fs.readdir(dir, (err, files) => {
-                async.each(files, (file, cb) => processDirectory(path.join(dir, file), cb), cb);
-            });
-        } else if (stats.isFile()) {
-            addFileToSizeBucket(dir, stats);
-            cb();
-        }
-    });
-}
 
 function addFileToSizeBucket(file, stats) {
     if (!fileMap.has(stats.size)) {
@@ -70,7 +54,7 @@ function findDuplicatesInBuckets(cb) {
             let bucket = [firstPath];
             async.eachSeries(bucketFiles, (secondPath, eachCb) => {
                 compare.compareFiles(firstPath, secondPath, fileSize, (err, areSame) => {
-                    if(areSame) {
+                    if (areSame) {
                         bucket.push(secondPath);
                     }
                     eachCb();
